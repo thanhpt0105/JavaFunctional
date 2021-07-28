4c59b21d9e4f3e384e1e4e166c78a5206ff7b3ee
thanhpt0105 <thanhpt0105@gmail.com>
2021-07-28 16:39:19 +0800

    Usage of BiFunction Interface

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,5 +1,6 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -14,12 +15,19 @@ public class _Function {
         int z = incrementByOne.andThen(multiplyBy10).apply(1);
         System.out.println(z);
 
+        int biX = incrementBy1ThenMultiplyByX.apply(2, 10);
+        System.out.println(biX);
+
     }
     static int increment(int number) {
         return number + 1;
     }
 
+    //Function interface take 1 argument and return 1 output
     static Function<Integer, Integer> incrementByOne = number -> number + 1;
     static Function<Integer, Integer> multiplyBy10 = number -> number * 10;
     static Function<Integer, Integer> incrementBy1ThenMultiplyBy10 = incrementByOne.andThen(multiplyBy10);
+
+    //BiFunction interface take 2 arguments and return 1 output
+    static BiFunction<Integer, Integer, Integer> incrementBy1ThenMultiplyByX = (number, x) -> (number + 1) * x;
 }
